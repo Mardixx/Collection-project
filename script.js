c5478b4c6070c7039452436451a29d1ce26fb8b1
Kimi L <kimi.lefort.pro@gmail.com>
2023-09-20 16:40:08 +0200

    Finishing touch to do

---
+++ script.js
@@ -0,0 +1,237 @@
+const object = [
+    {
+        // 1
+        divClass: 'one-piece',
+        animeName: 'One Piece',
+        genre: ['Adventure', 'Fantasy'],
+        author: 'Eichiro Oda',
+        image: 'imgs/OnePiece.jpe',
+        date: 'October 20, 1999',
+        description: "One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump since July 1997, with its individual chapters compiled into 106 tankōbon volumes as of July 2023. The story follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, where he explores the Grand Line in search of the mythical treasure known as the One Piece in order to become the next King of the Pirates.",
+        link : ['https://myanimelist.net/anime/21/One_Piece']   
+    },
+    {
+        // 2
+        divClass: 'bleach',
+        author: 'Tite Kubo',
+        animeName: 'Bleach',
+        genre: ['Adventure', 'Martial Arts', 'Supernatural'],
+        image: 'imgs/bleach.jpeg',
+        date: 'October 5, 2004',
+        description: "Bleach is a Japanese anime television series based on Tite Kubo's original manga series of the same name. It was produced by Studio Pierrot and directed by Noriyuki Abe. The series aired on TV Tokyo from October 2004 to March 2012, spanning 366 episodes. The story follows the adventures of Ichigo Kurosaki after he obtains the powers of a Soul Reaper—a death personification similar to the Grim Reaper—from another Soul Reaper, Rukia Kuchiki. His newfound powers force him to take on the duties of defending humans from evil spirits and guiding departed souls to the afterlife. In addition to adapting the manga series it is based on, the anime periodically includes original self-contained storylines and characters not found in the source material.",
+        link : ['https://myanimelist.net/anime/269/Bleach' ]    
+    },
+    {
+        // 3
+        divClass: 'darling',
+        author: 'Kentaro Yabuki',
+        animeName: 'Darling in the FranXX',
+        genre: ['Mecha', 'Romatical Drama'],
+        image: 'imgs/darlinginthefranxx.jpg',
+        date: 'January 13, 2018',
+        description: "Darling in the Franxx (Japanese: ダーリン・イン・ザ・フランキス, Hepburn: Dārin In Za Furankisu, stylized as DARLING in the FRANXX), abbreviated as DarliFra (ダリフラ, DariFura), is a 2018 Japanese science fiction monster romance anime television series co-produced by A-1 Pictures and Trigger and animated by Trigger and CloverWorks that premiered in January 2018. The series was announced at Trigger's Anime Expo 2017 panel in July 2017. A manga adaptation by Kentaro Yabuki and another four-panel comic strip manga began serialization in January 2018.",
+        link: ['https://myanimelist.net/anime/35849/Darling_in_the_FranXX']
+    },
+    {
+        // 4
+        divClass: 'kny',
+        author: 'Koyoharu Gotouge',
+        animeName: 'Kimetsu no Yaiba',
+        genre: ['Adventure', 'Martial Arts', 'Dark Fantasy'],
+        image: 'imgs/demonslayer.jpg',
+        date: 'April 6, 2019',
+        description: "Demon Slayer: Kimetsu no Yaiba (鬼滅の刃, Kimetsu no Yaiba, `Blade of Demon Destruction` is a Japanese manga series written and illustrated by Koyoharu Gotouge. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2016 to May 2020, with its chapters collected in 23 tankōbon volumes. It has been published in English by Viz Media and simultaneously published by Shueisha on their Manga Plus platform. It follows teenage Tanjiro Kamado, who strives to become a Demon Slayer after his family was slaughtered and his younger sister, Nezuko, is turned into a demon." ,
+        link: ['https://myanimelist.net/anime/38000/Kimetsu_no_Yaiba']
+    },
+    {
+        // 5
+        divClass: 'drstone',
+        author: 'Riichiro Inagak',
+        animeName: 'Dr. Stone',
+        genre: ['Adventure', 'Post Apocalyptic', 'Science Fiction'],
+        image: 'imgs/drstone.jpeg',
+        date: 'July 5, 2019',
+        link: ['https://myanimelist.net/anime/38691/Dr_Stone'],
+        description: "Dr. Stone is a Japanese manga series written by Riichiro Inagaki and illustrated by the South Korean artist Boichi. It was serialized in Shueisha's Weekly Shōnen Jump from March 2017 to March 2022, with its chapters collected in 26 tankōbon volumes. The story follows Senku Ishigami, a scientific genius who plans to rebuild civilization after humanity was mysteriously petrified for 3,700 years." 
+    },
+    {
+        // 6
+        divClass: 'naruto-shippuden',
+        author: 'Masashi Kishimoto',
+        animeName: 'Naruto: Shippuden',
+        genre: ['Adventure', 'Fantasy Comedy', 'Martial Arts'],
+        image: 'imgs/narutoshippuden.jpe',
+        date: 'July 5, 2019',
+        description: "Naruto Uzumaki, is a loud, hyperactive, adolescent ninja who constantly searches for approval and recognition, as well as to become Hokage, who is acknowledged as the leader and strongest of all ninja in the village." ,
+        link: ['https://myanimelist.net/anime/1735/Naruto__Shippuuden']
+    },
+    {
+        // 7
+        divClass: 'sword-art-online',
+        author: 'Reki Kawahara',
+        animeName: 'Sword Art Online',
+        genre: ['Adventure', 'Science Fiction'],
+        image: 'imgs/swordartonline.jpg',
+        date: 'July 8, 2012',
+        description: "Sword Art Online (Japanese: ソードアート・オンライン, Hepburn: Sōdo Āto Onrain) is a Japanese light novel series written by Reki Kawahara and illustrated by abec. The series takes place in the 2020s and focuses on protagonists Kazuto 'Kirito' Kirigaya and Asuna Yuuki as they play through various virtual reality MMORPG worlds, and later their involvement in the matters of a simulated civilization. Kawahara originally wrote the series as a web novel on his website from 2002 to 2008. The light novels began publication on ASCII Media Works' Dengeki Bunko imprint from April 10, 2009, with a spin-off series launching in October 2012. The series has spawned twelve manga adaptations published by ASCII Media Works and Kadokawa. The novels and the manga adaptations have been licensed for release in North America by Yen Press." ,
+        link: ['https://myanimelist.net/anime/11757/Sword_Art_Online']
+    },
+    {
+        // 8
+        divClass: 'danmachi',
+        author: 'Fujino Ōmori',
+        animeName: 'Is It Wrong to Try to Pick Up Girls in a Dungeon?',
+        genre: ['Fantasy', ' Sword and sorcery'],
+        image: 'imgs/danmachi.jpg',
+        date: 'April 4, 2015',
+        description: "Is It Wrong to Try to Pick Up Girls in a Dungeon? (Japanese: ダンジョンに出会いを求めるのは間違っているだろうか, Hepburn: Danjon ni Deai o Motomeru no wa Machigatteiru Darō ka), also known as DanMachi (ダンまち) for short, and with English subtitle Familia Myth, is a Japanese light novel series written by Fujino Ōmori and illustrated by Suzuhito Yasuda. SB Creative has published nineteen volumes since January 2013 under their GA Bunko imprint." ,
+        link: ['https://myanimelist.net/anime/28121/Dungeon_ni_Deai_wo_Motomeru_no_wa_Machigatteiru_Darou_ka']
+    },
+    {
+        // 9
+        divClass: 'koe-no-katachi',
+        author: 'Yoshitoki Ōima',
+        animeName: 'Koe no Katachi',
+        genre: ['Award winning', 'Drama'],
+        image: 'imgs/koenokatachi.jpg',
+        date: 'September 17, 2016',
+        description: "A Silent Voice (Japanese: 映画 聲の形, Hepburn: Eiga Koe no Katachi, lit. 'The Shape of Voice Film') is a 2016 Japanese animated drama film, based on the manga of the same name by Yoshitoki Ōima. The film was produced by Kyoto Animation, directed by Naoko Yamada and written by Reiko Yoshida, featuring character designs by Futoshi Nishiya and music by Kensuke Ushio. Plans for an animated film adaptation were announced back in November 2014, Kyoto Animation was confirmed to produce the film in November 2015. Miyu Irino and Saori Hayami signed on as voice casting in May 2016 and the theatrical release poster and official trailer were released in July 2016." ,
+        link: ['https://myanimelist.net/anime/28851/Koe_no_Katachi']
+    },
+    {
+        // 10
+        divClass: 'kimi-no-nawa',
+        author: 'Makoto Shinkai',
+        animeName: 'Kimi no Nawa',
+        genre: ['Award wining', 'Drama', 'Supernatural'],
+        image: 'imgs/kiminonawa.jpg',
+        date: 'August 26, 2016',
+        description: "Your Name (Japanese: 君の名は。, Hepburn: Kimi no Na wa) is a 2016 Japanese animated romantic fantasy film written and directed by Makoto Shinkai, produced by CoMix Wave Films and distributed by Toho. It depicts the story of high school students Taki Tachibana and Mitsuha Miyamizu, who suddenly begin to swap bodies despite having never met, unleashing chaos on each other's lives. The film was inspired by Japan's frequency for natural disasters." ,
+        link: ['https://myanimelist.net/anime/32281/Kimi_no_Na_wa']
+    },
+    {
+        // 11
+        divClass: 'black-clover',
+        author: 'Yūki Tabata',
+        animeName: 'Black Clover',
+        genre: ['Adventure', 'Fantasy'],
+        image: 'imgs/blackclover.jpe',
+        date: 'October 3, 2017',
+        description: "Black Clover (Japanese: ブラッククローバー, Hepburn: Burakku Kurōbā) is a Japanese manga series written and illustrated by Yūki Tabata. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2015 to August 2023, and will be moved to Jump Giga starting in its winter 2023 issue. Its chapters have been collected in 35 tankōbon volumes as of June 2023. The series left the magazine in August 2023 and will transfer to Jump Giga in December 2023 to finish its final arc. Set in a world where everyone is given the ability to use magic, the story follows Asta, a young boy born without any magic power who is given a rare grimoire that grants him anti-magic abilities. With his fellow mages from the Black Bulls, Asta plans to become the next Wizard King." ,
+        link: ['https://myanimelist.net/anime/34572/Black_Clover']
+    },
+    {
+        // 12
+        divClass: 'classroom-of-the-elite',
+        author: 'Shōgo Kinugasa',
+        animeName: 'Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e ',
+        genre: ['Psychological thriller'],
+        image: 'imgs/classroomoftheelite.jpg',
+        date: 'July 12, 2017',
+        description: "Classroom of the Elite (Japanese: ようこそ実力至上主義の教室へ, Hepburn: Yōkoso Jitsuryoku Shijō Shugi no Kyōshitsu e, lit. `Welcome to the Classrooms of Real Ability Supremacist`) is a Japanese light novel series, written by Shōgo Kinugasa and illustrated by Shunsaku Tomose published from May 2015 to September 2019 under Media Factory's MF Bunko J imprint. A sequel, Classroom of the Elite: Year 2, began publishing in January 2020. A manga adaptation by Yuyu Ichino began its serialization in Media Factory's Monthly Comic Alive on January 27, 2016. A manga adaptation of the sequel light novel series illustrated by Shia Sasane began serialization in the same magazine in December 2021. An anime television series adaptation produced by Lerche aired with the first season in 2017. A second season aired from July to September 2022, and a third season is set to premiere in January 2024." ,
+        link: ['https://myanimelist.net/anime/35507/Youkoso_Jitsuryoku_Shijou_Shugi_no_Kyoushitsu_e']
+    },
+    
+]
+
+for (let child of object) {
+
+    let collection = document.querySelector('.container')
+
+    let newDiv = document.createElement('div')
+
+    newDiv.classList.add(child.divClass)
+
+    let newImg = document.createElement('img')
+
+    newImg.src = child.image
+
+    newImg.style.width = '90%'
+
+    newImg.style.margin = '20px auto 10px auto'
+
+    newDiv.appendChild(newImg)
+
+    let buttonDiv = document.createElement('div')
+
+    let grandChild = child.genre;
+
+    buttonDiv.classList.add('buttonDiv')
+
+    for (let children of grandChild) {
+
+        let newButton = document.createElement('button')
+
+        let buttonText = document.createTextNode(children)
+
+        newButton.appendChild(buttonText)
+
+        buttonDiv.appendChild(newButton)
+
+        newDiv.appendChild(buttonDiv)
+    }
+
+    let newspanName = document.createElement('span')
+
+    newspanName.classList.add('name')
+
+    let newnameText = document.createTextNode(child.animeName)
+
+    newspanName.appendChild(newnameText)
+
+    newDiv.appendChild(newspanName)
+
+    let newspanAuthor = document.createElement('span')
+
+    newspanAuthor.classList.add('author')
+
+    let newauthorText = document.createTextNode(child.author)
+
+    newspanAuthor.appendChild(newauthorText)
+
+    newDiv.appendChild(newspanAuthor)
+
+    let newspanDate = document.createElement('span')
+
+    let newdateText = document.createTextNode(child.date)
+
+    newspanDate.appendChild(newdateText)
+
+    newDiv.appendChild(newspanDate)
+
+    let newP = document.createElement('p')
+
+    let description = document.createTextNode(child.description)
+
+    newP.appendChild(description)
+
+    newDiv.appendChild(newP)
+
+    let bigChild = child.link;
+
+    let malDiv = document.createElement('div')
+
+    let malSVG = document.createElement('img')
+
+    let malA = document.createElement('a')
+
+    for (let child of bigChild) {
+        malA.href = child
+
+        console.log(child);
+
+        malA.target = '_blank'
+
+        malA.appendChild(malSVG)
+
+        malSVG.src = 'imgs/myanimelist.svg'
+
+        malSVG.style.width = '25%'
+
+        malDiv.appendChild(malA)
+
+        newDiv.appendChild(malDiv)
+    }
+
+    collection.appendChild(newDiv)
+}
